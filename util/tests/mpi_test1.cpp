36a778ce97988b2954536f189f284570b742ba28
jwhogabo <justin.w.hogaboam@intel.com>
2017-04-03 09:47:36 -0700

    Updates to some of the test functions.

--- util/tests/mpi_test1.cpp
+++ util/tests/mpi_test1.cpp
@@ -26,49 +26,66 @@
  *
  */
 #include "../mpi_wrapper.hpp"
-#include "openmp_affinity_corei7.hpp"
+#include <omp.h>
 #include <stdio.h>
 #include <stdexcept>
-
-#ifndef _OPENMP
-qhipster::openmp::AffinityNoOmp affinity;
-#else
-qhipster::openmp::AffinityCoreI7 affinity;
-#endif
+#include <cassert>
 
 
 int main(int argc, char **argv) {
 
     int world_size = 0;
     int world_rank = 0;
+    int my_node_size = 0;
+    int my_node_rank = 0;
     char processor_name[MPI_MAX_PROCESSOR_NAME];
     int name_len;
 
-    int max_threads = omp_get_max_threads();
-    printf("Max available threads %d\n", max_threads);
-
-    affinity.set_thread_affinity(2);
-
     // Initialize the MPI Framework.
-    MPI_Init(NULL,NULL);
+    qhipster::MpiWrapper mpi(argc,argv);
 
-    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
-    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
-    MPI_Get_processor_name(processor_name, &name_len);
+    QH_MPI_STATUS_CHECK((MPI_Comm_size(MPI_COMM_WORLD, &world_size)));
+    QH_MPI_STATUS_CHECK((MPI_Comm_rank(MPI_COMM_WORLD, &world_rank)));
+    QH_MPI_STATUS_CHECK((MPI_Get_processor_name(processor_name, &name_len)));
+
+    printf("World Rank/Size [%2d:%-2d]\n", world_rank, world_size);
+
+    // Split the communicator into subcommunicators which have the capability of creating
+    // shared memory.
+    MPI_Comm node_comm;
+    QH_MPI_STATUS_CHECK(MPI_Comm_split_type(MPI_COMM_WORLD, MPI_COMM_TYPE_SHARED, 
+                        world_rank, MPI_INFO_NULL, &node_comm));
+
+    // Determine the number of ranks in the new node we just split and also
+    // my rank within it.
+    QH_MPI_STATUS_CHECK(MPI_Comm_size(node_comm, &my_node_size));
+    QH_MPI_STATUS_CHECK(MPI_Comm_rank(node_comm, &my_node_rank));
+
+    printf("Group Rank/Node#: [%2d/%-2d]\n", my_node_rank, my_node_size / world_size);
+
+    // This should detect non-symmetric partitioning of the processes into
+    // uniform sized groups of nodes. For example: World: 16 processes,
+    // 4 processes per comm group, 0 left-over processes.
+    assert((world_size % my_node_size) == 0);
+
+    // Partition the available global ranks into communication groups that are an even multiple of 2.
+    // Left-over ranks stay in the global rank pool.
+    
+
+
+
+
 
 #pragma omp parallel
     {
         int threadID = omp_get_thread_num();
-        printf("GlbID: [%d] Thread [%d]\n", world_rank, threadID);
+        printf("Node ID/Rank: [%2d/%-2d] Thread [%d] working...\n", my_node_size/world_size, my_node_rank, threadID);
 
-        int a = 0,b=0,c=0;
+        int a=0,b=0,c=0;
         for(unsigned long i=0;i<2000000000;i++) {
             unsigned long a = (++b/2) + (++c/2);
         }
     }
-
-    // Clean-up the MPI Framework...we're done.
-    MPI_Finalize();
 
     return 1;
 }
